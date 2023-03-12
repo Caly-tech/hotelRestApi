58c7992bc7c8aa6ef98249fd13982eb423ab7129
kepsonDiaz <kherabadiaby328@gmail.com>
2023-03-12 19:44:08 +0000

    ajout des routes pour voir une chambre et mettre à jour ses infos

--- main.go
+++ main.go
@@ -19,9 +19,10 @@ func main() {
 
 	r := gin.Default()
 	r.POST("/createCategories", controllers.CreateCategoriesController) // route pour ajouter une categorie
-	r.GET("/showChambres", controllers.ShowChambresController) // route pour Lister les chambres
 	r.POST("/createChambres", controllers.CreateChambresController) // route pour ajouter une Chambre 
-
+	r.GET("/showChambres", controllers.ShowChambresController) // route pour Lister les chambres
+	r.GET("/showChambres/:id", controllers.ShowIDChambresController) //route pour lister une chambre en fonction de son id 
+	r.PUT("/showChambres/:id", controllers.UpdateChambresController)
 
 	r.Run() // listen and serve on 127.0.0.0:3000
 }
